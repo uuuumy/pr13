c4aab560015f75e6579ea056c272e35e43814cad
uuuumy <e.idanova@g.nsu.ru>
2026-05-16 15:49:31 +0700

    Update 13.10.py

--- 13.10.py
+++ 13.10.py
@@ -1,32 +1,23 @@
 def threeSum(nums):
-    """Returns triples"""
     nums.sort()
-    result = []
+    result = set()
     n = len(nums)
 
     for i in range(n - 2):
-        if i > 0 and nums[i] == nums[i - 1]:
-            continue
-
         j = i + 1
         k = n - 1
+
         while j < k:
-            total = nums[i] + nums[j] + nums[k]
-
-            if total < 0:
+            s = nums[i] + nums[j] + nums[k]
+            if s == 0:
+                result.add((nums[i], nums[j], nums[k]))
                 j += 1
-            elif total > 0:
                 k -= 1
+            elif s < 0:
+                j += 1
             else:
-                result.append([nums[i], nums[j], nums[k]])
-                while j < k and nums[j] == nums[j + 1]:
-                    j += 1
-                while j < k and nums[k] == nums[k - 1]:
-                    k -= 1
-
-                j += 1
                 k -= 1
 
-    return result
+    return [list(_) for _ in result]
 
-print(threeSum([-1, 0, 1, 2, -1, -4]))
+print(threeSum([-1,0,1,2,-1,-4]))
